008bf6d333a3f7355d0657d3bc8efb0557ceb36b
Jean Oliveira <jcwilbertt@gmail.com>
2023-07-12 00:30:04 +0100

    update

--- 1.introducao_python/29.1_funcoes.py
+++ 1.introducao_python/29.1_funcoes.py
@@ -1,4 +1,9 @@
-def soma_numeros(n1, n2):
-    print(n1 + n2)
+def soma_numeros(**kwargs):
+    x = kwargs.get('test6')
+    if x:
+        print('Foi passado')
+        print(x)
+    else:
+        print('Não foi passado')
 
-soma_numeros(5,2)
+soma_numeros(test1 = 1, test2 = 2, test3 = 3)
