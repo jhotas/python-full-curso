243b8918a943da29682939a81a9a2ffa35a9a0f4
Jean Oliveira <jcwilbertt@gmail.com>
2023-07-11 14:18:56 +0100

    funcoes.py

---
+++ 1.introducao_python/29.1_funcoes.py
@@ -0,0 +1,4 @@
+def soma_numeros(n1, n2):
+    print(n1 + n2)
+
+soma_numeros(5,2)
---
+++ 1.introducao_python/29_funcoes.py
@@ -0,0 +1,7 @@
+def minha_funcao():
+    soma = 0
+    for i in range(0, 101):
+      soma = soma + 1
+    print(soma)
+
+minha_funcao()
