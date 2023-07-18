9a152376b9d451f349017bf2f36df440b235f560
Jean Oliveira <jcwilbertt@gmail.com>
2023-07-18 14:54:54 +0100

    treino

--- 1.introducao_python/treino.py
+++ 1.introducao_python/treino.py
@@ -1,16 +1,20 @@
-print('--- PESQUISAR PESSOAS ---')
-print('Selecione uma opção: \n1. Adicionar nome\n2. Consultar nome\n3. Encerrar')
-opcao = int(input(''))
-
 nomes = []
 def adicionar_nome(nome):
     nomes.append(nome)
     return
 
-if opcao == 1:
-    addnome = input('Digite o nome: ')
-    adicionar_nome(addnome)
-elif opcao == 2:
-    pesqnome = input('Pesquise o nome: ')
-    nomes = list(filter(lambda nomes: nomes == pesqnome))
-    print(nomes)
+def inicio():
+    print('\n--- PESQUISAR PESSOAS ---')
+    print('Selecione uma opção: \n1. Adicionar nome\n2. Consultar nome\n3. Encerrar')
+    opcao = int(input(''))
+    if opcao == 1:
+        addnome = input('Digite o nome: ')
+        adicionar_nome(addnome)
+        print(nomes)
+        return inicio()
+    
+inicio()
+
+#pesqnome = input('Pesquise o nome: ')
+#nomes = list(filter(lambda nomes: nomes == pesqnome, nomes))
+#print(nomes)
