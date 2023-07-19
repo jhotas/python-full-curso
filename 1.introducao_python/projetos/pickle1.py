d67656d12159b104130abefe63c3c0afcf5c4274
Jean Oliveira <jcwilbertt@gmail.com>
2023-07-19 16:03:59 +0100

    pickle

--- 1.introducao_python/projetos/pickle1.py
+++ 1.introducao_python/projetos/pickle1.py
@@ -1,5 +1,20 @@
 import pickle
-x = [1, 2, 3, 4]
 
-string = pickle.dumps(x)
-print(pickle.loads(string))
+class Pessoa:
+    nome = 'Jean'
+    idade = 21
+
+class Pessoas:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+p1 = Pessoas('Johan', 12)
+
+arq = open('arquivo.pkl', 'wb')
+pickle.dump(p1, arq)
+
+arq = open('arquivo.pkl', 'rb')
+retornou = pickle.load(arq)
+
+print(retornou.nome)
