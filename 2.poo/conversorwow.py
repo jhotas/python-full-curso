bbd005463d5d798cbd803e6606cfdc7f036a22d6
Jean Oliveira <jcwilbertt@gmail.com>
2023-07-23 19:13:47 +0100

    sobreposicao_metodos

--- 2.poo/conversorwow.py
+++ 2.poo/conversorwow.py
@@ -1,18 +1,13 @@
-# Not working yet
 cobre = float(input('Digite a quantidade de cobre: '))
 prata = float(input('Digite a quantidade de prata: '))
 ouro = float(input('Digite a quantidade de ouro: '))
 
-cobre_conv = 0
-prata_conv = 0
-ouro_conv = 0
+while cobre >= 100:
+    prata += 1
+    cobre -= 100
 
-if cobre > 100:
-    cobre_conv = cobre / 100
-    prata_final = cobre_conv
+while prata >= 100:
+    ouro += 1
+    prata -= 100
 
-if prata > 100:
-    prata_conv = prata / 100
-    ouro_final = prata_conv
-
-print(f'Total: {ouro_final}G {prata_final}P {cobre}')
+print(f'Total: {ouro}G {prata}P {cobre}C')
