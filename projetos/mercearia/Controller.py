5de1aa34d9cf396481031436be67857603410c9f
Jean Oliveira <jcwilbertt@gmail.com>
2023-07-26 16:28:57 +0100

    Controller

---
+++ projetos/mercearia/Controller.py
@@ -0,0 +1,3 @@
+from Models import Categoria, Estoque, Produtos, Fornecedor, Pessoa, Funcionario, Venda
+from DAO import DaoCategoria, DaoVenda, DaoEstoque, DaoFornecedor, DaoPessoa, DaoFuncionario
+from datetime import datetime
