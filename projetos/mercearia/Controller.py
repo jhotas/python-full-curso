31a42d79c30b35820bbbec745322ec53d3a5cf07
Jean Oliveira <jcwilbertt@gmail.com>
2023-07-26 16:45:20 +0100

    cadastro_remover_categorias

--- projetos/mercearia/Controller.py
+++ projetos/mercearia/Controller.py
@@ -1,3 +1,35 @@
 from Models import Categoria, Estoque, Produtos, Fornecedor, Pessoa, Funcionario, Venda
 from DAO import DaoCategoria, DaoVenda, DaoEstoque, DaoFornecedor, DaoPessoa, DaoFuncionario
 from datetime import datetime
+
+class ControllerCategoria:
+    def cadastrarCategoria(self, novaCategoria):
+        existe = False
+        x = DaoCategoria.ler()
+        for i in x:
+            if i.categoria == novaCategoria:
+                existe = True
+
+        if not existe:
+            DaoCategoria.salvar(novaCategoria)
+            print('Categoria cadastrada com sucesso!')
+        else:
+            print('A categoria que deseja cadastrar já existe')
+
+    def removerCategoria(self, categoriaRemover):
+        x = DaoCategoria.ler()
+        cat = list(filter(lambda x: x.categoria == categoriaRemover, x))
+
+        if len(cat) <= 0:
+            print('A categoria que deseja remover não existe')
+        else:
+            for i in range(len(x)):
+                if x[i].categoria == categoriaRemover:
+                    del x[i]
+                    break
+            print('Categoria removida com sucesso!')
+
+            with open('categoria.txt', 'w') as arq:
+                for i in x:
+                    arq.writelines(i.categoria)
+                    arq.writelines('\n')
